4f16195f550e0b202bfa20075e1c2e7819cf8483
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-09-20 17:08:38 +0900

    added some property token related definitions.
    NOTE: it’s not working yet.

--- muscr/include/parser.h
+++ muscr/include/parser.h
@@ -17,7 +17,12 @@ using namespace boost::spirit;
 
 enum TokenId
 {
-    ID_ANY = lex::min_token_id + 10
+    ID_PROPERTY_MARK = lex::min_token_id + 10,
+    ID_PROPERTY_VALUE,
+    ID_IDENTIFIER,
+    ID_COLON,
+    ID_EOL,
+    ID_ANY
 };
 
 template <typename Lexer>
@@ -27,9 +32,18 @@ struct MuscrTokens : lex::lexer<Lexer>
             : MuscrTokens::base_type(lex::match_flags::match_default)
     {
         // define tokens and associate them with the lexer
-        lineComment_ = "\"--\"[^\n]*";       // '--[^\n]*'
-        blockCommentStart_ = "\"{--\"";      // '{--'
-        blockCommentEnd_ = "\"--}\"";        // '--}'
+        lineComment_ = "\"--\"[^\n]*";              // '--[^\n]*'
+        blockCommentStart_ = "\"{--\"";             // '{--'
+        blockCommentEnd_ = "\"--}\"";               // '--}'
+
+        propertyMark_ = "^\"@\"";                   // '^@'
+        propertyValue_ = "[^\n]*";
+
+        identifier_ = "[a-zA-Z_][a-zA-Z0-9_']*";
+
+        colon_ = "\":\"";
+
+        eol_ = "\"\n\"";
 
         // The following tokens are associated with the default lexer state
         // (the "INITIAL" state). Specifying 'INITIAL' as a lexer state is
@@ -37,6 +51,7 @@ struct MuscrTokens : lex::lexer<Lexer>
         this->self.add
                 (lineComment_)    // no explicit token id is associated
                 (blockCommentStart_)
+                //(propertyMark_)
                 (".", ID_ANY)    // ID_ANY is the token id associated with this token
                                  // definition
         ;
@@ -48,11 +63,24 @@ struct MuscrTokens : lex::lexer<Lexer>
                 (blockCommentEnd_)
                 (".", ID_ANY)
         ;
+
+        this->self("PROPERTY").add
+                (identifier_)
+                (colon_)
+                (propertyValue_)
+                (eol_)
+        ;
     }
 
     lex::token_def<> lineComment_;
     lex::token_def<> blockCommentStart_;
     lex::token_def<> blockCommentEnd_;
+
+    lex::token_def<> propertyMark_;
+    lex::token_def<> propertyValue_;
+    lex::token_def<> identifier_;
+    lex::token_def<> colon_;
+    lex::token_def<> eol_;
 };
 
 
@@ -66,13 +94,23 @@ struct MuscrGrammar : qi::grammar<Iterator>
         // The in_state("COMMENT")[...] parser component switches the lexer
         // state to be 'COMMENT' during the matching of the embedded parser.
         start_ = *(   tok.blockCommentStart_
-                              >>  qi::in_state("COMMENT")
+                              >> qi::in_state("COMMENT")
                                     [
                                       // the lexer is in the 'COMMENT' state during
                                       // matching of the following parser components
                                       *token(ID_ANY) >> tok.blockCommentEnd_
                                     ]
                   |   tok.lineComment_
+                      /*
+                  |   tok.propertyMark_
+                              >> qi::in_state("PROPERTY")
+                                    [
+                                        tok.identifier_
+                                                >> tok.colon_
+                                                >> tok.propertyValue_
+                                                >> tok.eol_
+                                    ]
+                                    */
                   |   qi::token(ID_ANY)  [ std::cout << _1 ]
                   )
         ;
