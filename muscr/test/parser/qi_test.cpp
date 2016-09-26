2f737bb3135dc92f4a9a8b94d6d46419b8b003f0
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-09-26 11:35:34 +0900

    updated the test case.

--- muscr/test/parser/qi_test.cpp
+++ muscr/test/parser/qi_test.cpp
@@ -28,9 +28,11 @@ TEST_CASE("num_list1", "[qi]")
     std::string s0 = "11, 22, 33";
     std::string s1 = "11 22, 33";
     std::string s2 = "abc, 11, zz";
+    std::string s3 = "11  , 22, 33";
     REQUIRE(num_list_parser(s0.begin(), s0.end()));
     REQUIRE(!num_list_parser(s1.begin(), s1.end()));
     REQUIRE(!num_list_parser(s2.begin(), s2.end()));
+    REQUIRE(num_list_parser(s3.begin(), s3.end()));
 }
 
 TEST_CASE("num_list2", "[qi]")
