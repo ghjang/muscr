b3680cb8e34924fcd61c54101b5c00210df24c2e
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-12-19 03:53:52 +0900

    added a test case for property match.

--- muscr/test/parser/leadsheet_property_test.cpp
+++ muscr/test/parser/leadsheet_property_test.cpp
@@ -185,3 +185,52 @@ TEST_CASE("simple match - 3", "[leadsheet property]")
     REQUIRE(propVec[6].name_ == "bpm");
     REQUIRE(propVec[6].value_ == " 100");
 }
+
+TEST_CASE("simple match - 4", "[leadsheet property]")
+{
+    using namespace boost::spirit;
+    using qi::ascii::alpha;
+    using qi::ascii::char_;
+    using qi::ascii::blank;
+    using qi::eol;
+    using qi::lexeme;
+
+    std::vector<property> propVec;
+
+    auto prop_matcher = [&propVec](auto begin, auto end) {
+        bool r = qi::phrase_parse(
+                        begin,
+                        end,
+                        *('@' >> +alpha >> ':' >> lexeme[+(char_ - eol)]),
+                        qi::ascii::space,
+                        propVec
+                );
+        return (r && begin == end);
+    };
+
+    std::string str{
+        R"(
+
+@title: A Sample Song
+@author: ghjang
+
+@scale: C Major
+@pitchRange: 3
+@clef: G
+
+@timeSignature: 4 / 4
+@bpm: 100)"     // NOTE: no newline at the last property.
+    };
+    REQUIRE(prop_matcher(str.begin(), str.end()));
+    REQUIRE(propVec.size() == 7);
+
+    REQUIRE(boost::fusion::at_c<0>(propVec[0]) == "title");
+    REQUIRE(boost::fusion::at_c<1>(propVec[0]) == "A Sample Song");
+    REQUIRE(boost::fusion::at_c<0>(propVec[6]) == "bpm");
+    REQUIRE(boost::fusion::at_c<1>(propVec[6]) == "100");
+
+    REQUIRE(propVec[0].name_ == "title");
+    REQUIRE(propVec[0].value_ == "A Sample Song");
+    REQUIRE(propVec[6].name_ == "bpm");
+    REQUIRE(propVec[6].value_ == "100");
+}
