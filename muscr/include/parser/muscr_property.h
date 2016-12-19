ac23c8eab5f705d8f1aa898216e0af8e72fe084e
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-12-19 17:37:21 +0900

    closes #2: - added semantic actions to check the duplicate property names.

--- muscr/include/parser/muscr_property.h
+++ muscr/include/parser/muscr_property.h
@@ -3,9 +3,16 @@
 
 
 #include <cstdint>
+#include <set>
 
 #include <boost/fusion/adapted/struct/define_struct.hpp>
 #include <boost/fusion/include/define_struct.hpp>
+
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
+
 #include <boost/spirit/include/qi.hpp>
 
 
@@ -46,14 +53,61 @@ namespace muscr
         using qi::eol;
         using qi::lexeme;
 
-        template <typename Iterator>
-        qi::rule<Iterator, muscr::property(), qi::ascii::space_type> prop_str_val{
+        template <typename Iterator, typename SpaceType>
+        qi::rule<Iterator, muscr::property(), SpaceType> prop_str_val{
             '@' >> muscr::prop_name<Iterator> >> ':' >> lexeme[+(char_ - eol)]
         };
     } // namespace detail
 
-    template <typename Iterator>
-    auto prop_str_val{ detail::prop_str_val<Iterator> };
+    template <typename Iterator, typename SpaceType = boost::spirit::qi::ascii::space_type>
+    auto prop_str_val{ detail::prop_str_val<Iterator, SpaceType> };
+
+    template <typename Iterator, typename SpaceType = boost::spirit::qi::ascii::space_type>
+    struct global_properties
+            : boost::spirit::qi::grammar<
+                    Iterator,
+                    std::vector<muscr::property>(),
+                    boost::spirit::qi::locals<std::string>,
+                    SpaceType
+              >
+    {
+        global_properties()
+            : global_properties::base_type(start_)
+        {
+            namespace qi = boost::spirit::qi;
+            using qi::eps;
+            using namespace qi::labels;
+            namespace phx = boost::phoenix;
+            using phx::at_c;
+            using phx::insert;
+            using phx::push_back;
+            using phx::count;
+
+            property_ = prop_str_val<Iterator, SpaceType>;
+
+            // NOTE: if a semantic action is attached to a parser,
+            //       then no properties are emitted automatically anymore.
+            start_ = *(property_[
+                            _a = at_c<0>(_1),
+                            insert(phx::ref(propNames_), at_c<0>(_1)),
+                            push_back(_val, _1) // need to push it manually.
+                       ]
+                       >> eps(
+                            count(phx::ref(propNames_), _a) == 1
+                          )
+                      );
+        }
+
+        boost::spirit::qi::rule<Iterator, muscr::property(), SpaceType> property_;
+        boost::spirit::qi::rule<
+            Iterator,
+            std::vector<muscr::property>(),
+            boost::spirit::qi::locals<std::string>,
+            SpaceType
+        > start_;
+
+        std::multiset<std::string> propNames_;
+    };
 } // namespace muscr
 
 
