9202dcaf9f4b1c86ee54b8f66b862192134df408
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-12-22 01:48:14 +0900

    added empty files for muscr leadsheet rule.

---
+++ muscr/include/parser/muscr_leadsheet.h
@@ -0,0 +1,15 @@
+#ifndef MUSCR_LEADSHEET_H
+#define MUSCR_LEADSHEET_H
+
+
+#include "muscr/include/parser/muscr_property.h"
+#include "muscr/include/parser/muscr_section.h"
+
+
+namespace muscr
+{
+    
+} // namespace muscr
+
+
+#endif // MUSCR_LEADSHEET_H
---
+++ muscr/test/parser/leadsheet_test.cpp
@@ -0,0 +1,9 @@
+#include "../catch.hpp"
+
+#include "muscr/include/parser/muscr_leadsheet.h"
+
+
+TEST_CASE("leadsheet match", "[leadsheet]")
+{
+
+}
