8c2d1768587052dd1a42bc2e9af93b8b80eef990
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-12-22 02:37:28 +0900

    fixed broken build.

--- muscr/test/parser/leadsheet_score_test.cpp
+++ muscr/test/parser/leadsheet_score_test.cpp
@@ -1,6 +1,6 @@
 #include "../catch.hpp"
 
-#include "muscr/include/parser/muscr_leadsheet.h"
+#include "muscr/include/parser/muscr_leadsheet_score.h"
 
 
 TEST_CASE("leadsheet score match", "[leadsheet score]")
