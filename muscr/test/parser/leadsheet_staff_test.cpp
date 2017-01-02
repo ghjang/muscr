161bf8ce1b5e664f2293ab33de4ceeeda9f1bd76
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-03 07:44:31 +0900

    added leadsheet staff attribute test case.

--- muscr/test/parser/leadsheet_staff_test.cpp
+++ muscr/test/parser/leadsheet_staff_test.cpp
@@ -80,5 +80,47 @@ A ==
 
 TEST_CASE("leadsheet staff attr", "[leadsheet staff]")
 {
-    
+    using tools::test_phrase_parser_attr;
+
+    std::string staffs[] = {
+R"(
+@title: A Sample Song
+@author: ghjang
+
+@scale: C Major
+@pitchRange: 3
+@clef: G
+
+@timeSignature: 4 / 4
+@bpm: 100
+
+A :=
+{
+    C, D | E, (B, G) | A   | A, C
+    C    | Em        | Am  | F
+}
+)"
+    };
+
+    muscr::parser::leadsheet_staff<std::string::iterator> staff_;
+    muscr::parser::leadsheet_staff_attr attr;
+
+    for (auto & s : staffs) {
+        REQUIRE(test_phrase_parser_attr(
+                    s,
+                    staff_,
+                    attr
+                ));
+    }
+
+    REQUIRE("A Sample Song" == attr.properties_["title"]);
+    REQUIRE("ghjang" == attr.properties_["author"]);
+    REQUIRE("C Major" == attr.properties_["scale"]);
+    REQUIRE("3" == attr.properties_["pitchRange"]);
+    REQUIRE("G" == attr.properties_["clef"]);
+    REQUIRE("4 / 4" == attr.properties_["timeSignature"]);
+    REQUIRE("100" == attr.properties_["bpm"]);
+
+    REQUIRE(1 == attr.sections_.size());
+    REQUIRE("A" == attr.sections_[0].name_);
 }
