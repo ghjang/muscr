a2e09654fcf79c912df990b33626b06f5a12749f
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-03 05:09:02 +0900

    made rules and attributes separate.

--- muscr/include/generator/leadsheet_js_song.h
+++ muscr/include/generator/leadsheet_js_song.h
@@ -2,96 +2,11 @@
 #define MUSCR_GENERATOR_LEADSHEET_JS_SONG_H
 
 
-#include <cstdint>
 #include <string>
-#include <vector>
-
-#include <boost/fusion/adapted/struct/define_struct.hpp>
-#include <boost/fusion/include/define_struct.hpp>
 
 #include <boost/spirit/include/karma.hpp>
 
-
-namespace muscr
-{
-    namespace ljs
-    {
-        struct chord_attr
-        {
-            std::string pitchClass_;
-            std::string modifier_;              // major modifiers like 'm', 'aug' and 'dim'.
-            std::string accessaryModifier_;     // accessary modifiers like '7', 'M7', '9' etc.
-            std::uint8_t beatPos_;
-        };
-
-        struct note_attr
-        {
-            std::string pitchClass_;
-            std::int8_t octavePos_;
-            std::string duration_;
-        };
-
-        struct bar_attr
-        {
-            std::vector<chord_attr> chords_;
-            std::vector<note_attr> notes_;
-        };
-
-        struct section_attr
-        {
-            std::string name_;
-            std::vector<bar_attr> bars_;
-        };
-
-        struct leadsheet_attr
-        {
-            std::string title_;
-            std::string composer_;
-            std::string keySignature_;
-            std::string time_;
-            std::vector<section_attr> sections_;
-        };
-    }
-}
-
-// NOTE: BOOST_FUSION_DEFINE_STRUCT was NOT WORKING in the nested namespace like the muscr::ljs.
-//          Need to re-test it?
-
-BOOST_FUSION_ADAPT_STRUCT(
-    muscr::ljs::chord_attr,
-    (std::string, pitchClass_)
-    (std::string, modifier_)
-    (std::string, accessaryModifier_)
-    (std::uint8_t, beatPos_)
-)
-
-BOOST_FUSION_ADAPT_STRUCT(
-    muscr::ljs::note_attr,
-    (std::string, pitchClass_)
-    (std::int8_t, octavePos_)
-    (std::string, duration_)
-)
-
-BOOST_FUSION_ADAPT_STRUCT(
-    muscr::ljs::bar_attr,
-    (std::vector<muscr::ljs::chord_attr>, chords_)
-    (std::vector<muscr::ljs::note_attr>, notes_)
-)
-
-BOOST_FUSION_ADAPT_STRUCT(
-    muscr::ljs::section_attr,
-    (std::string, name_)
-    (std::vector<muscr::ljs::bar_attr>, bars_)
-)
-
-BOOST_FUSION_ADAPT_STRUCT(
-    muscr::ljs::leadsheet_attr,
-    (std::string, title_)
-    (std::string, composer_)
-    (std::string, keySignature_)
-    (std::string, time_)
-    (std::vector<muscr::ljs::section_attr>, sections_)
-)
+#include "muscr/include/generator/leadsheet_js_song_attr.h"
 
 
 namespace muscr
