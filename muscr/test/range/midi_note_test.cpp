2b7baae2c84f713def038c50f8cd2b03e5147943
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-13 16:14:24 +0900

    updated test cases.

--- muscr/test/range/midi_note_test.cpp
+++ muscr/test/range/midi_note_test.cpp
@@ -5,9 +5,18 @@
 #include "muscr/include/range/midi_note.h"
 
 
-TEST_CASE("midi note range", "[range]")
+TEST_CASE("midi note number", "[range]")
+{
+    using muscr::range::midi_note_number;
+
+    midi_note_number n{ 60 };
+    REQUIRE(60 == n);
+}
+
+TEST_CASE("midi note number range view", "[range]")
 {
     using namespace ranges;
+    using muscr::range::midi_note_number;
     using muscr::range::midi_note_number_range_view;
     using number_t = midi_note_number_range_view::number_type;
 
@@ -28,4 +37,10 @@ TEST_CASE("midi note range", "[range]")
     REQUIRE(12 == middleNotes1.size());
     REQUIRE(60 == middleNotes1.front());
     REQUIRE(71 == middleNotes1.back()); // NOTE: it's still 71.
+
+    midi_note_number_range_view notes3{ 60, 71 }; // from middle C to B
+    std::vector<midi_note_number> middleNotes2 = notes2 | view::take(24); // used midi_note_number instead.
+    REQUIRE(12 == middleNotes2.size());
+    REQUIRE(60 == middleNotes2.front());
+    REQUIRE(71 == middleNotes2.back()); // NOTE: it's still 71.
 }
