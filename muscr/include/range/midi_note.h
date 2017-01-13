208ac0894b2e8ba5e6d85ecd9b419c0d1a1dace4
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-13 16:00:42 +0900

    added struct midi_note_number_range_view: - added test case for it.

---
+++ muscr/include/range/midi_note.h
@@ -0,0 +1,71 @@
+#ifndef MUSCR_MIDI_NOTE_H
+#define MUSCR_MIDI_NOTE_H
+
+
+#include <cassert>
+#include <cstdint>
+
+#include <range/v3/all.hpp>
+
+
+struct midi_note_number_range_view
+        : ranges::view_facade<midi_note_number_range_view>
+{
+public:
+    using number_type = std::uint8_t;
+
+public:
+    midi_note_number_range_view(number_type start = 0, number_type end = 127)
+        : start_{ start }
+        , end_{ end }
+    {
+        assert(start >= 0 && start <= 127);
+        assert(end >= 0 && end <= 127);
+        assert(start <= end);
+    }
+
+private:
+    number_type const start_;
+    number_type const end_;
+
+private:
+    friend struct ranges::range_access;
+
+    struct cursor
+    {
+        number_type get() const
+        { return n_; }
+
+        bool equal(cursor const& rhs) const
+        { return n_ == rhs.n_; }
+
+        void next()
+        { ++n_; }
+
+        void prev()
+        { --n_; }
+
+        std::ptrdiff_t distance_to(cursor const& rhs) const
+        { return rhs.n_ - n_; }
+
+        void advance(std::ptrdiff_t n)
+        { n_ += n; }
+
+        number_type n_;   
+    };
+
+    cursor begin_cursor() const
+    {
+        assert(start_ <= end_);
+        return { start_ };
+    }
+
+    cursor end_cursor() const
+    {
+        assert(start_ <= end_);
+        return { static_cast<number_type>(end_ + 1) };
+    }
+};
+
+
+#endif // MUSCR_MIDI_NOTE_H
---
+++ muscr/test/range/midi_note_test.cpp
@@ -0,0 +1,30 @@
+#include "../catch.hpp"
+
+#include <vector>
+
+#include "muscr/include/range/midi_note.h"
+
+
+TEST_CASE("midi note range", "[range]")
+{
+    using namespace ranges;
+    using number_t = midi_note_number_range_view::number_type;
+
+    midi_note_number_range_view notes;
+    std::vector<number_t> first10Notes = notes | view::take(10);
+    REQUIRE(10 == first10Notes.size());
+    REQUIRE(0 == first10Notes.front());
+    REQUIRE(9 == first10Notes.back());
+
+    midi_note_number_range_view notes1{ 60 }; // middle C
+    std::vector<number_t> middleNotes = notes1 | view::take(12);
+    REQUIRE(12 == middleNotes.size());
+    REQUIRE(60 == middleNotes.front());
+    REQUIRE(71 == middleNotes.back());
+
+    midi_note_number_range_view notes2{ 60, 71 }; // from middle C to B
+    std::vector<number_t> middleNotes1 = notes2 | view::take(24);
+    REQUIRE(12 == middleNotes1.size());
+    REQUIRE(60 == middleNotes1.front());
+    REQUIRE(71 == middleNotes1.back()); // NOTE: it's still 71.
+}
