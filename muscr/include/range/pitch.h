472241052ad788a0fce95ba63adee47e19f835f9
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-14 08:10:21 +0900

    - made pitch_class default constructable. - moved free conversion functions to a separate file, transform.h.

--- muscr/include/range/pitch.h
+++ muscr/include/range/pitch.h
@@ -19,7 +19,7 @@ namespace muscr::range
     public:
         using number_type = std::uint8_t;
 
-        pitch_class(number_type pc)
+        pitch_class(number_type pc = 0)
             : pc_{ pc }
         {
             assert(pc >= 0 && pc <= 11);
@@ -50,65 +50,8 @@ namespace muscr::range
         number_type pc_;
     };
 
-    pitch_class to_pitch_class(std::string const& s)
-    {
-        using str_to_pc_map_t = std::map<std::string, pitch_class::number_type>;
-        static str_to_pc_map_t const map{
-            { "C",      0 },
-            { "C#",     1 },    { "DB",    1 },
-            { "D",      2 },
-            { "D#",     3 },    { "EB",    3 },
-            { "E",      4 },
-            { "F",      5 },
-            { "F#",     6 },    { "GB",    6 },
-            { "G",      7 },
-            { "G#",     8 },    { "AB",    8 },
-            { "A",      9 },
-            { "A#",     10 },   { "BB",     10},
-            { "B",      11 }
-        };
 
-        std::string key = boost::to_upper_copy(s);
-        auto iter = map.find(key);
-        if (map.end() == iter) {
-            // TODO: throw an exception
-        }
-        return pitch_class{ iter->second };
-    }
-
-    std::string to_string(pitch_class const& pc, bool useFlat = true)
-    {
-        using pc_to_str_map_t = std::map<pitch_class::number_type, std::string>;
-        static pc_to_str_map_t const map{
-            { 0,    "C"     },
-            { 1,    "Db"    },
-            { 2,    "D"     },
-            { 3,    "Eb"    },
-            { 4,    "E"     },
-            { 5,    "F"     },
-            { 6,    "Gb"    },
-            { 7,    "G"     },
-            { 8,    "Ab"    },
-            { 9,    "A"     },
-            { 10,   "Bb"    },
-            { 11,   "B"     }
-        };
-
-        auto iter = map.find(pc);
-        if (map.end() == iter) {
-            // TODO: throw an exception
-        }
-        if (useFlat || iter->second.length() == 1) {
-            return iter->second;
-        }
-        assert(iter->second.length() == 2);
-        iter = map.find(pc - 1);
-        if (map.end() == iter) {
-            // TODO: throw an exception
-        }
-        assert(iter->second.length() == 1);
-        return std::string{ iter->second.front(), '#' };
-    }
+    inline pitch_class to_pitch_class(std::string const& s);
 
     struct pitch_class_view
             : ranges::view_facade<pitch_class_view>
